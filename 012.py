61f234e95c6aa93d61010d364c5922b26c48a7bc
Victor Vahid <victorvahid76@example.com>
2024-08-09 09:23:47 -0400

    012✅

--- 012.py
+++ 012.py
@@ -1,6 +1,7 @@
 # construa um algoritmo que calcule seu peso ideal,
 # usando a seguinte fórmula: (72.7*altura) - 58
 
+
 altura = float(input('Digite sua altura em metros: '))
 peso = (72.7 *altura) - 58
 print(f'Peso idela é: {peso:.2f}')
