d66cd5e92abbc329dec312fe726579f79a01c612
Victor Vahid <victorvahid76@example.com>
2024-08-09 09:22:46 -0400

    008

--- 008.py
+++ 008.py
@@ -1,6 +1,7 @@
 #Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
 #Calcule e mostre o total do seu salário no referido mês.
 
+
 salario_hora = float(input('Digite seu salário hora: '))
 horas_trabalhadas = float(input('Digite horas trabalhadas no mês: '))
 
