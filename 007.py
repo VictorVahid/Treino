67b78fda5a28db5dbcd8f0eda175359fe3c9738f
Victor Vahid <victorvahid76@example.com>
2024-08-09 09:22:40 -0400

    007✅

--- 007.py
+++ 007.py
@@ -1,5 +1,6 @@
 #Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
 
+
 lado = int(input('Digite o valor do lado do quadrado: '))
 
 area= lado**2
