b5860a4a3b9912673136698fb7f50a9cf7e3e25a
Victor Vahid <victorvahid76@example.com>
2024-08-12 21:15:07 -0400

    ❌

---
+++ 017.py
@@ -0,0 +1,25 @@
+
+"""
+
+O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. ✅
+
+Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados
+
+tinta é vendida em latas de 18 litros = R$ 80,00
+galões de 3,6 litros =  R$ 25,00.
+
+Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
+
+"""
+
+metros_quadrados = float(input('Informe  em m² da área a ser pintada: '))
+
+rendimento_litro = 6
+litros_lata = 18
+preco_lata = 80
+rendimento_lata = rendimento_litro * litros_lata
+litros_galao = 3.6
+preco_galao = 25
+rendimento_galao = rendimento_litro * litros_galao
+
+...
