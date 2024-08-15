841016e55b81f5b60135daa0ee81a8a8b3ec9c2b
Victor Vahid <victorvahid76@example.com>
2024-08-15 09:25:37 -0400

    ✅

---
+++ 026.py
@@ -0,0 +1,8 @@
+produto_1 = float(input('Digite o valor do produto 1: '))
+produto_2 = float(input('Digite o valor do produto 2: '))
+produto_3 = float(input('Digite o valor do produto 3: '))
+
+
+menor = min(produto_1,produto_2,produto_3)
+
+print(f'Compre o produto {menor}')
